bf4188659feae05f1c6100bcc065b5d6d55e32a7
Matt Mower <mowerm@gmail.com>
2014-11-04 22:19:49 -0600

    dlx: utilize recovery_root_private
    Prebuilt modules can be placed in the correct directory structure under
    recovery/root and the build system will automatically copy them to the
    recovery ramdisk.
    
    Change-Id: Icc99d1dcd5b952c7f0356bd7cb3386f727bf62a8

--- full_dlx.mk
+++ full_dlx.mk
@@ -38,18 +38,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.post_boot.sh:system/etc/init.post_boot.sh
 
-# Recovery
-PRODUCT_PACKAGES += \
-    lpm.rc \
-    choice_fn \
-    detect_key \
-    power_test \
-    chargeled \
-    offmode_charging \
-    offmode_charging_res_images \
-    offmode_charging_warn_res_images \
-    init.recovery.dlx.rc
-
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
