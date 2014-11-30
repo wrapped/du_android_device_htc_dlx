LineageOS/android_device_htc_dlx

sepolicy: add device-specific sepolicies
Change-Id: Ia337b3448f20e8e77ea0c4d07a0d7090d7ba34f7

BoardConfig.mk
@@ -80,6 +80,30 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 # Use libril in the device tree
 BOARD_PROVIDES_LIBRIL := true
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/htc/dlx/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    akmd.te \
+    device.te \
+    drmserver.te \
+    file_contexts \
+    hcheck.te \
+    init.te \
+    init_shell.te \
+    kickstart.te \
+    mediaserver.te \
+    mm-qcamerad \
+    mpdecision.te \
+    netmgrd.te \
+    qcks.te \
+    qmuxd.te \
+    qrngd.te \
+    surfaceflinger.te \
+    tee.te \
+    thermald.te \
+    ueventd.te
+
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 

wrapped/du_android_device_htc_dlx

BoardConfig.mk
@@ -80,6 +80,31 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 # Use libril in the device tree
 BOARD_PROVIDES_LIBRIL := true
 
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += device/htc/dlx/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    akmd.te \
+    device.te \
+    drmserver.te \
+    file_contexts \
+    hcheck.te \
+    init.te \
+    init_shell.te \
+    mediaserver.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    netmgrd.te \
+    qmuxd.te \
+    surfaceflinger.te \
+    system_server.te \
+    tee.te \
+    thermald.te \
+    ueventd.te \
+    vold.te \
+    wpa.te
+
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 