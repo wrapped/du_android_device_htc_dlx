b98281403fb136aceaf85128697366eb335f8854
wrapped <ddroidx@gmail.com>
2015-11-20 21:02:01 -0600

    dlx: make it dirty

--- du.mk
+++ du.mk
@@ -8,18 +8,18 @@ PRODUCT_RELEASE_NAME := dlx
 # overlays
 PRODUCT_PACKAGE_OVERLAYS += device/htc/dlx/overlay
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/dlx/full_dlx.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlx
-PRODUCT_NAME := cm_dlx
+PRODUCT_NAME := du_dlx
 PRODUCT_BRAND := Verizon
 PRODUCT_MODEL := HTC6435LVW
 PRODUCT_MANUFACTURER := HTC
