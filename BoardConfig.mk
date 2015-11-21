fdf828c10f89b9251b7373b32e9be3beae358dc4
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-21 14:27:35 -0600

    dlx: remove custom ril class
    Change-Id: I1da1cd94fa0004ad596fbc670376dcc28070fc84

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,6 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
-BOARD_RIL_CLASS := ../../../device/htc/dlx/ril
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
